6ef0a645c95c9e793389020933481f32d999ce2f
gabriel schiavo <gabriel@lewagon.org>
2019-10-07 00:14:48 -0300

    add local method for manipulate UI

--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -5,18 +5,27 @@ using UnityEngine.UI;
 
 public class UIScript : RandomItemGenerator
 {
-	 public Text itemNameText;
-   public Text itemDescriptionText;
+	 public Text NameText;
+   public Text DescriptionText;
+   public Text PowerText;
+   public Text DimensionText;
+   public Button GetItem;
     // Start is called before the first frame update
     void Start()
     {
      	// Teste do método pai (RandomItemGenerator)
-     	Debug.Log(GetItemName());
+     	GetItem.onClick.AddListener(GetItem);
+     	// Debug.Log(GetItemName());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void GetItem()
+    {
+        NameText.text = 
     }
 }

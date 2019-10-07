f6a2e072e8e42010b04ac8d97fed25f873437740
gabriel schiavo <gabriel@lewagon.org>
2019-10-06 23:48:15 -0300

    add method call on void Start()

--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -10,7 +10,7 @@ public class UIScript : RandomItemGenerator
     // Start is called before the first frame update
     void Start()
     {
-        
+     	GetRandomItem();   
     }
 
     // Update is called once per frame

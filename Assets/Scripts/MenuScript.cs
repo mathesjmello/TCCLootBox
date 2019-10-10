c056b756d12232bc6f7b99ad43d8eb99ebe95548
gabriel schiavo <gabriel@lewagon.org>
2019-10-10 15:49:56 -0300

    finish function for material

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -1,9 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class MenuScript
 {
     // Start is called before the first frame update
-    
+    [MenuItem("Tools/Assing Tile Material")]
+    public void AssingTileMaterial()
+    {
+    	GameObject[] tiles = GameObject.GameObject.FindGameObjectWithTag("Tile");
+    	Material material = Resources.Load<Material>("Tile");
+
+    	foreach (GameObject t in tiles)
+    	{
+    		t.GetComponent<Renderer>().material = material;
+    	}
+    }
 }

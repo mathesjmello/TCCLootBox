e9c9f5f08f606039e1e83e629ace820904abafb6
gabriel schiavo <gabriel@lewagon.org>
2019-10-10 17:34:06 -0300

    add gameObject List

--- Assets/Resources/TaticsMove.cs
+++ Assets/Resources/TaticsMove.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class TaticsMove : MonoBehaviour
 {
     List<TileScript> selectableTiles = new List<TileScript>();
-    
+    GameObject[] tiles;
+
+    Stack<TileScript> path = new Stack<TileScript>();
+    TileScript currentTile;
+
     public int move = 3;
     public float jumpHeight = 2;
+    public float moveSpeed = 2;
 
     Vector3 velocity = new Vector3();
     Vector3 pointVector = new Vector3();
 
+    float halfHeight = 0;
+
     protected void Init()
     {
+    	tiles = GameObject.FindGameObjectsWithTag("Tile");
 
+    	halfHeight = 
     }
 }

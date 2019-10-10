6e25059e195146ee49df4d3c1b0d4debb687d473
gabriel schiavo <gabriel@lewagon.org>
2019-10-10 16:13:20 -0300

    assing functions for btn

---
+++ Assets/Resources/TacticsCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TacticsCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void RotateLeft()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void RotateRight()
+    {
+        
+    }
+}

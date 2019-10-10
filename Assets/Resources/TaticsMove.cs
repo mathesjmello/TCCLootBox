ba05cfdaa99f38a39dd622ff71bbbe950a9318ed
gabriel schiavo <gabriel@lewagon.org>
2019-10-10 17:29:59 -0300

    picking some player variables

--- Assets/Resources/TaticsMove.cs
+++ Assets/Resources/TaticsMove.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class TaticsMove : MonoBehaviour
 {
+    List<TileScript> selectableTiles = new List<TileScript>();
     
+    public int move = 3;
+    public float jumpHeight = 2;
+
+    Vector3 velocity = new Vector3();
+    Vector3 pointVector = new Vector3();
+
     protected void Init()
     {
 

98591bcf479d94187a6f3f57eb9f2427f793f9bf
matheus <junqueirademello@gmail.com>
2019-10-12 15:59:23 -0300

    marge pause menu

---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SoundController : MonoBehaviour
+{
+    public AudioMixer MasterMixer;
+
+    public Slider MasterSlider;
+    public Slider MusicSlider;
+    public Slider SfxSlider;
+    public Slider DialogSlider;
+
+    // Start is called before the first frame update
+    
+    public void UpdateMasterVolume()
+    {
+        MasterMixer.SetFloat("MasterV", MasterSlider.value * 20);
+    }
+    public void UpdateMusicVolume(float sliderValue)
+    {
+        MasterMixer.SetFloat("MusicaV", MusicSlider.value * 20);
+    }
+    public void UpdateSfxVolume(float sliderValue)
+    {
+        MasterMixer.SetFloat("SfxV", SfxSlider.value * 20);
+    }
+    public void UpdateDialogVolume(float sliderValue)
+    {
+        MasterMixer.SetFloat("DialogV", DialogSlider.value * 20);
+    }
+}

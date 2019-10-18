6c4091143141f87d8ae032bed79d56a370b528bf
gabriel schiavo <gabriel@lewagon.org>
2019-10-18 10:13:19 -0300

    add team count

--- Assets/Resources/RoundManager.cs
+++ Assets/Resources/RoundManager.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public class RoundManager : MonoBehaviour
 {
-    static Dictionary<string, List<TacticsMove>> units = new Dictionary<string, List<TacticsMove>>(); 
+    static Dictionary<string, List<TaticsMove>> units = new Dictionary<string, List<TaticsMove>>(); 
+    static Queue<string> turnkey = new Queue<string>();
+    static Queue<TaticsMove> turnTeam = new Queue<TaticsMove>();
+
+
+    void Update()
+    {
+    	if (turnTeam.Count == 0)
+    	{
+    		InitTeamTurnQueue();
+    	}
+    }
+    static void InitTeamTurnQueue()
+    {
+    	List<TaticsMove> teamList = units[turnkey.Peek()];
+
+    	foreach ( TaticsMove unit in teamList )
+    	{
+    		turnTeam.Enqueue(unit);
+    	}
+    }
     
 }

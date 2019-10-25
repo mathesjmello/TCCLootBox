99f54c9ddbff4667f1efcee58b4ba634968b8501
gabriel schiavo <gabriel@lewagon.org>
2019-10-25 19:19:51 -0300

    add script for control item sorting

---
+++ Assets/Resources/ItemControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class ItemControl : MonoBehaviour
+{
+    public static event Action HandlePulled = delegate { };
+    
+    [SerializeField]
+    private Text prizeText;
+    [SerializeField]
+    private Row[] rows;
+    
+    private int prizeValue;
+    private bool resultsChecked = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!rows[0].rowStopped || !rows[1].rowStopped)
+        {
+        	prizeValue = 0;
+        	prizeText.enable = false;
+        	resultsChecked = false;
+        }
+
+        if (rows[0].rowStopped && rows[1].rowStopped)
+    }
+}

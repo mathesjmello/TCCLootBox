4b992d8761f1dc5f72e6113902d070f8d2b22644
gabriel schiavo <gabriel@lewagon.org>
2019-11-01 19:01:47 -0300

    add health script

---
+++ Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem 
+{
+    public int health;
+    // Start is called before the first frame update
+    public HealthSystem(int health) 
+    {
+    	this.health = health
+    } 
+    
+    public int GetHealth() {
+    	return health;
+    }
+
+    public void Damage(int damageAmount)
+    {
+    	health += damageAmount;
+    }
+}

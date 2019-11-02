b941368d30ba409a23890d0d89ea13f42ebc3e08
gabriel schiavo <gabriel@lewagon.org>
2019-11-02 14:44:09 +0100

    add function for get localScale from bar

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class HealthBar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private HealthSystem healthSystem;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void Setup(HealthSystem healthSystem){
+    	this.healthSystem = healthSystem;
+    }
+    private void Update() {
+    	transform.Find("Bar").localScale = new Vector3(healthSystem.GetHealthPercent(), 1);
     }
 }

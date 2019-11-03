0886ddbbcc7f2428ea8d610b68324dca72eab6fe
gabriel schiavo <gabriel@lewagon.org>
2019-11-03 10:53:22 +0000

    bug n destroy

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -9,16 +9,16 @@ public class HealthBar : MonoBehaviour
     public void Setup(HealthSystem healthSystem){
     	this.healthSystem = healthSystem;
 
-    	healthSystem.OnHealthChange += HealthSystem_OnHealthChange;
+    	// healthSystem.OnHealthChange += HealthSystem_OnHealthChange;
     }
     
-    private void HealthSystem_OnHealthChange(object sender, System.EventArgs e)
-    {
-    	transform.Find("Bar").localScale = new Vector3(healthSystem.GetHealthPercent(), 1);
-    }
-
-    // private void Update() 
+    // private void HealthSystem_OnHealthChange(object sender, System.EventArgs e)
     // {
     // 	transform.Find("Bar").localScale = new Vector3(healthSystem.GetHealthPercent(), 1);
     // }
+
+    private void Update() 
+    {
+    	transform.Find("Bar").localScale = new Vector3(healthSystem.GetHealthPercent(), 1);
+    }
 }

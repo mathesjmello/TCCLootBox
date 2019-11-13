18427c11aa17b90a4f8c59ae4aefa315bff6d962
ViniciusLN <vin.nascimento@outlook.com>
2019-11-13 07:34:14 -0300

    Nova Arte Livre e Cameras prontas

---
+++ Assets/Scripts/OpenMap/CamerasControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerasControl : MonoBehaviour
+{
+    public GameObject CameraCidade;
+    public GameObject CameraJogador ;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    public virtual void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            CameraCidade.SetActive(true);
+           // CameraJogador.SetActive(false);
+        }
+
+    }
+    public virtual void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //CameraJogador.SetActive(true);
+            CameraCidade.SetActive(false);
+            
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

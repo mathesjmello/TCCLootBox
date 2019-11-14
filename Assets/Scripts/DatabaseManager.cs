f21383f10f88a00c478d4a9e1cf96c8e04be4246
gabriel schiavo <gabriel@lewagon.org>
2019-11-14 00:01:18 +0000

    add binary definition for unity connect with DB

---
+++ Assets/Scripts/DatabaseManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite;
+
+public class DatabaseManager : MonoBehaviour
+{
+    private string databaseString;
+    // Start is called before the first frame update
+    void Start()
+    {
+        databaseString = "URI=file:" + Application.dataPath + "/DB/database.sqlite";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

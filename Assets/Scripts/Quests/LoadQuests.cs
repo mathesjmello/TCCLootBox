af629b07e58238a98c26d35c1ea91df45daadf49
gabriel schiavo <gabriel@lewagon.org>
2019-11-15 13:40:50 +0000

    parse all the rows from csv file

--- Assets/Scripts/Quests/LoadQuests.cs
+++ Assets/Scripts/Quests/LoadQuests.cs
@@ -12,10 +12,22 @@ public class LoadQuests : MonoBehaviour
      	string[] data = questdata.text.Split(new char[] { '\n' });
 
      	for (int i = 1; i < data.Length -1; i++){
+     		
      		string[] row = data[i].Split(new char[] { ',' });
+     		
      		Quest q = new Quest();
+     		
      		q.id = row[0];
      		int.TryParse(row[0] out q.id);
+     		q.name = row[1];
+     		q.npc = row[2];
+     		q.desc = row[3];
+
+     		int.TryParse(row[4] out q.status);
+     		q.rewards = row[5];
+     		q.task = row[6];
+     		int.TryParse(row[7], out q.parent);
+
      	}
     }
 

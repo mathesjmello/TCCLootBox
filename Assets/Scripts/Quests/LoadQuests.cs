add8a5f1edda9486bd31ac96fbec8347f2c94a0f
gabriel schiavo <gabriel@lewagon.org>
2019-11-15 13:26:13 +0000

    add path for parse quests

--- Assets/Scripts/Quests/LoadQuests.cs
+++ Assets/Scripts/Quests/LoadQuests.cs
@@ -7,7 +7,7 @@ public class LoadQuests : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+     	TextAsset questdata = Resources.Load<TextAsset>("../DB/csv/quests");
     }
 
     // Update is called once per frame

c51f21ba08112f2f4b56646bfa922549fa738943
Gabriel <gabs_gginez@outlook.com>
2019-11-25 10:06:07 -0300

    Sword fix

--- Assets/Sprites/Ari Battle-20191124T211759Z-001/Ari Battle/Espada girando/ProjectileSword.cs
+++ Assets/Sprites/Ari Battle-20191124T211759Z-001/Ari Battle/Espada girando/ProjectileSword.cs
@@ -6,7 +6,13 @@ public class ProjectileSword : MonoBehaviour
 {
 
     public GameObject enemie;
-    static float t = 0.0f;
+    public Transform Player;
+    public Transform Enemie;
+
+    [Range(0f, 1f)]
+    public float lerpPct = 0.0f;
+
+    //static float t = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +23,18 @@ public class ProjectileSword : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Mathf.Lerp(transform.position.x, enemie.transform.position.x, t), 0, Mathf.Lerp(transform.position.z, enemie.transform.position.z, t));
+        transform.position = Vector3.Lerp(Player.position, Enemie.position, lerpPct);
 
-        t += 10000000000000000000 * Time.deltaTime;
+        if (lerpPct < 1.0f)
+        {
+            lerpPct += 0.1f;
+        }
+        else if (lerpPct >= 1.0f)
+        {
+            Destroy(gameObject);
+        }
+        //transform.Translate(Mathf.Lerp(transform.position.x, enemie.transform.position.x, t), 0, Mathf.Lerp(transform.position.z, enemie.transform.position.z, t));
+
+        //t += 10000000000000000000 * Time.deltaTime;
     }
 }

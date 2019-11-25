81edd65ccd64fb07042a22610c9e9d1aee2797e2
Gabriel <gabs_gginez@outlook.com>
2019-11-25 09:13:47 -0300

    Sword Anim

--- Assets/Sprites/Ari Battle-20191124T211759Z-001/Ari Battle/Espada girando/ProjectileSword.cs
+++ Assets/Sprites/Ari Battle-20191124T211759Z-001/Ari Battle/Espada girando/ProjectileSword.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class ProjectileSword : MonoBehaviour
 {
+
+    public GameObject enemie;
+    static float t = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,8 @@ public class ProjectileSword : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Mathf.Lerp(transform.position.x, enemie.transform.position.x, t), 0, Mathf.Lerp(transform.position.z, enemie.transform.position.z, t));
+
+        t += 10000000000000000000 * Time.deltaTime;
     }
 }

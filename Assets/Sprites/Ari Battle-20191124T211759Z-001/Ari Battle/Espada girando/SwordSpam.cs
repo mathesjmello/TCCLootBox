cb7e9f63caf71d409a6f694026f991680093836a
Matheus <matheus.mello@vetorzero.com.br>
2019-11-25 11:43:05 -0300

    arrumando spada por instanciamento

---
+++ Assets/Sprites/Ari Battle-20191124T211759Z-001/Ari Battle/Espada girando/SwordSpam.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordSpam : MonoBehaviour
+{
+
+    public GameObject SwordProjectPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InstantiateSword()
+    {
+        Instantiate(SwordProjectPrefab, transform.parent);
+    }
+}

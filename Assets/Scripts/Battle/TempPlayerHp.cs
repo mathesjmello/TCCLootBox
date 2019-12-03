9ee88381cad02ecf6e23b917c5f588e7ec30d454
gabriel schiavo <gabriel@lewagon.org>
2019-12-03 03:51:38 -0300

    Finish Player healthbar

--- Assets/Scripts/Battle/TempPlayerHp.cs
+++ Assets/Scripts/Battle/TempPlayerHp.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TempPlayerHp : MonoBehaviour
 {
@@ -11,6 +12,15 @@ public class TempPlayerHp : MonoBehaviour
     public GameObject coll03;
     public GameObject coll04;
     public int PlayerHealth;
+    public Image LifeBar;
+    public float tempLife;
+    float waitForSec = 5.0f;
+    
+    public Animator slimeAnimator;
+     
+    // void Awake () {
+    //  slimeAnimator = slimeObject.GetComponent<Animator> ();
+    // }
 
     // Start is called before the first frame update
     void Start()
@@ -21,19 +31,34 @@ public class TempPlayerHp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        LifeCheck();
+        Debug.Log("P. Health: " + PlayerHealth);              
     }
 
     private void OnTriggerEnter(Collider col)
     {
         if (col == coll01 || col == coll02 || col == coll03 || col == coll04)
         {
-            PlayerHealth--;
+            Debug.Log("Collision");
+
+
+
             if (PlayerHealth <= 0)
             {
                 PlayerPrefs.SetString("_sceneName", NextCenaName);
                 LoadingSisten.LoadLevel(NextCenaName);
             }
         }
+    }
+
+    public void LifeCheck()
+    {
+        
+        if( slimeAnimator.GetCurrentAnimatorStateInfo(0).IsName("GS_Attack"))
+         {
+                PlayerHealth--;    
+                LifeBar.fillAmount -= 1.0f / waitForSec * Time.deltaTime;
+         }
+        
     }
 }

d1c20385aaa6b43d95bb2e0e980c76480e122f62
gabriel schiavo <gabriel@lewagon.org>
2020-01-08 16:02:31 -0300

    Init LeanTween

---
+++ Assets/Scripts/UITweener.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITweener : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     	LeanTween.scale(gameObject, new Vector3(0,0,0), 0.5f);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

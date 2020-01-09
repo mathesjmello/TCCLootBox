aba6c0a73844da34f68ac64e253689a336c7b260
gabriel schiavo <gabriel@lewagon.org>
2020-01-09 13:42:13 -0300

    📝 Add transition for optionsMenu

--- Assets/Scripts/UITweener.cs
+++ Assets/Scripts/UITweener.cs
@@ -7,12 +7,17 @@ public class UITweener : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-     	LeanTween.scale(gameObject, new Vector3(0,0,0), 0.5f);   
+     	LeanTween.scale(gameObject, new Vector3(0,0,0), 0.5f).setOnComplete(DestroyMe);   
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void DestroyMe()
+    {
+    	Destroy(gameObject);
     }
 }

2f5d9e893560c32ac7ab2a35fb27d128c6949631
gabriel schiavo <gabriel@lewagon.org>
2020-03-05 18:12:49 -0300

    add some Hotbar logic

--- Assets/Scripts/Item/HotbarManager.cs
+++ Assets/Scripts/Item/HotbarManager.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class HotbarManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int HotbarSlotSize => gameObject.transform.childCount;
+    private List<ItemSlot> hotbarSlots = new List<ItemSlot>();
+
+    KeyCode[] hotbarKeys = new KeyCode[] { KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4, KeyCode.Alpha5};
+    
+    private void Start()
     {
-        
+        SetupHotbarSlots();
+    }
+    
+    private void Update()
+    {
+        for(int i = 0; i < hotbarKeys.Length; i++)
+        {
+            if(Input.GetKeyDown(hotbarKeys[i]))
+            {
+                Debug.Log("Use item: " + i);
+                // Use item
+                return;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetupHotbarSlots()
     {
-        
+        for(int i = 0; i < HotbarSlotSize; i++)
+        {
+            ItemSlot slot = gameObject.transform.GetChild(i).GetComponent<ItemSlot>();
+            hotbarSlots.Add(slot);
+        }
     }
 }
